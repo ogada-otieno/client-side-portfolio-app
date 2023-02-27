089bee8ab442b070b5cb09440168a55f3c76ac3d
Rodgers Ogada <ogadarodgers@gmail.com>
2023-02-27 22:09:03 +0300

    Added Login form

---
+++ src/Components/LoginForm.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+function LoginForm() {
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    console.log({ ...register("email") });
+    reset();
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control">
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            {...register("email", {
+              required: "Email is required.",
+              pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+              message: "Email is not valid",
+            })}
+          />
+          {errors.email && <p className="errorMsg">{errors.email.message}</p>}
+          {errors.email?.type === "pattern" && (
+            <p className="errorMsg">Email is not valid.</p>
+          )}
+        </div>
+        <div className="form-control">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            {...register("password", {
+              required: "Password is required.",
+              minLength: {
+                value: 6,
+                message: "Password must be atleast 6 characters.",
+              },
+            })}
+          />
+          {errors.password && (
+            <p className="errorMsg">{errors.password.message}</p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p className="errorMsg">
+              Password should be at-least 6 characters.
+            </p>
+          )}
+        </div>
+        <div className="form-control">
+          <label></label>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
