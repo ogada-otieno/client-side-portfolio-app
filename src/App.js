7c321ed94bfc45647ba0ca01ea85d8787b25bb65
Rodgers Ogada <ogadarodgers@gmail.com>
2023-03-03 11:35:51 +0300

    Handle all routing (all are public as of now.)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
-// import LoginForm from './Components/LoginForm';
-import SignupForm from './Components/SignupForm';
+import LoginForm from "./Components/LoginForm";
+import SignupForm from "./Components/SignupForm";
+import Projects from "./Components/Projects";
+import Profile from "./Components/Profile";
+import NotFound from "./Components/NotFound";
+import Navbar from "./Pages/Navbar";
+import Footer from "./Pages/Footer";
+import Home from "./Pages/Home";
+import LandingPage from "./Pages/LandingPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      {/* <LoginForm /> */}
-      <SignupForm />
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <div className="app-body">
+        <Routes>
+          <Route exact path="/login" element={<LoginForm />} />
+          <Route exact path="/signup" element={<SignupForm />} />
+          <Route exact path="/projects" element={<Projects />} />
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="*" element={<NotFound />} />
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/home" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
