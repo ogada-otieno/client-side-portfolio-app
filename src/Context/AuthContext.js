fb827611647bd903010de3c2d6df2b8fa5853af0
Rodgers Ogada <ogadarodgers@gmail.com>
2023-03-05 14:04:41 +0300

    react app is able to check if the user is logged in in the browser and persist the user information in the global state/context

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 export const AuthContext = createContext();
 
@@ -16,11 +16,21 @@ export const authReducer = (state, action) => {
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, { user: null });
 
+  // ensuring that the user is logged in
+  // react app is able to check if the user is logged in in the browser and persist the user information in the global state/context
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user) {
+      dispatch({ type: "LOGIN", payload: user });
+    }
+  }, []);
+
   console.log("AuthContext state", state);
 
   return (
-    <AuthContext.Provider value={{...state, dispatch}}>
-    {children}
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
     </AuthContext.Provider>
-  )
+  );
 };
--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   const handleClick = () => {
     logout();
@@ -13,13 +15,17 @@ const Navbar = () => {
     <header>
       <div className="nav-container">
         <nav>
-          <div>
-            <button onClick={handleClick}>Log out</button>
-          </div>
-          <div>
+          {user && (
+            <div>
+            <span>Email: {user.email}</span>
+            <span>User ID: {user.user_id}</span>
+              <button onClick={handleClick}>Log out</button>
+            </div>
+          )}
+          {!user && <div>
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
-          </div>
+          </div>}
         </nav>
       </div>
     </header>
