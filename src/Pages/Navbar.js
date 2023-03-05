ba7800d13209ceb8d7ad1b22f1f4744226d2bfd5
Rodgers Ogada <ogadarodgers@gmail.com>
2023-03-05 12:46:13 +0300

    logic for user logout by removing user from local storage and dispatching logout action

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -1,19 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
+  const { logout } = useLogout();
+
+  const handleClick = () => {
+    logout();
+  };
+
   return (
     <header>
       <div className="nav-container">
         <nav>
           <div>
-            <Link to='/login'>Login</Link>
-            <Link to='/signup'>Signup</Link>
+            <button onClick={handleClick}>Log out</button>
+          </div>
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
           </div>
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
